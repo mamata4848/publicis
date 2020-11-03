d2e2757605bcf6c3e4fe1ff46024247b9ef265b1
Mamata <mamatha4848@gmail.com>
2020-11-03 18:20:12 +0530

    second commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -21,8 +21,6 @@ export default class Home extends React.Component{
    options = {
      2006: '2006', 2007:'2007', 2008:'2008',2009:'2009',2010:'2010',2011:'2011',2012:'2012',2013:'2013',2014:'2014',2015:'2015',2016:'2016',2017:'2017',2018:'2018',2019:'2019',2020:'2020'
    }
- 
- 
   landOptionSelectedTrue = (e) =>{
 	  alert()
 		var getLandVal= e.target.value
